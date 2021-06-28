56d735b941d780b6399f2e8ab84c081ffac7f6b4
Leland Mckibben <lmckibben28@hotmail.com>
2021-06-28 20:14:03 +0000

    Made a function to greet anyone

---
+++ hello.js
@@ -0,0 +1,15 @@
+/*
+const sayHello = function () {
+  console.log("Hello, World");
+}
+
+sayHello();
+*/
+
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
